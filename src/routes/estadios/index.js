8761b00b89c07e0d2b98069d6d4d86980665d748
santiagogarcia97 <santiagogarcia97@outlook.com>
2019-08-28 11:22:02 -0300

    GetAll de estadios

---
+++ src/routes/estadios/index.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+
+router.use(require('./estadiosGetAll'));
+
+module.exports = router;
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 router.use('/equipos', require('./equipos'));
 router.use('/jugadores', require('./jugadores'));
+router.use('/estadios', require('./estadios'));
 
 
 module.exports = router;
