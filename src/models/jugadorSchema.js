61846156b21a622aa843b899096df6865ea87a5e
santiagogarcia97 <santiagogarcia97@outlook.com>
2019-09-17 20:53:58 -0300

    Agregado esquema Joi a jugador

--- src/models/jugadorSchema.js
+++ src/models/jugadorSchema.js
@@ -2,23 +2,35 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const jugadorSchema = new Schema({
-    fechaNac: {
-        type: Date,
-        default: Date()
-    },
-    equipo: {
-        type: Schema.Types.ObjectId,
-        ref: 'equipos',
-        default: null
-    },
-    goles: {
-        type: Number,
-        default: 0
-    },
-    nombre: {
-        type: String,
-        required: true
-    }
+  nombre: {
+    type: String,
+    required: true
+  },
+  fechaNac: {
+    type: Date,
+    default: Date()
+  },
+  equipo: {
+    type: Schema.Types.ObjectId,
+    ref: 'equipos',
+    default: null
+  },
+  goles: {
+    type: Number,
+    default: 0
+  }
 });
+
+jugadorSchema.statics.joiValidate = (obj) => {
+  let Joi = require('@hapi/joi');
+  let schema = {
+    nombre:  Joi.string().min(3).max(30).required(),
+    fechaNac: Joi.date().max(Date()),
+    equipo: Joi.string().regex('/^[a-fA-F0-9]{24}$/'),
+    goles: Joi.number().integer().min(0)
+  }
+
+  return Joi.validate(obj, schema);
+}
 
 module.exports = mongoose.model('jugador', jugadorSchema, 'jugadores');
