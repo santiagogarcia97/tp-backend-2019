11b9ad51ee51ecccc86502412d6ae1557a1e37dc
santiagogarcia97 <santiagogarcia97@outlook.com>
2019-09-18 20:40:29 -0300

    getAll de dts

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 router.use('/api', require('./estadios'));
 router.use('/api', require('./jugadores'));
 router.use('/api', require('./tipoEvento'));
+router.use('/api', require('./dts'));
 
 module.exports = router;
