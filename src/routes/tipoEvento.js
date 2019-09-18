1c00e518ef6ff820b8a19321186ca8920e41e28a
santiagogarcia97 <santiagogarcia97@outlook.com>
2019-09-18 20:14:37 -0300

    update de tipos de evento

--- src/routes/tipoEvento.js
+++ src/routes/tipoEvento.js
@@ -4,7 +4,7 @@ const ctrlTipoEvento = require('../controllers/tipoEvento');
 router.post('/tipoevento', ctrlTipoEvento.create);
 router.get('/tipoevento', ctrlTipoEvento.getAll);
 router.get('/tipoevento/:id', ctrlTipoEvento.getById);
-//router.put('/tipoevento/:id', ctrlTipoEvento.update);
+router.put('/tipoevento/:id', ctrlTipoEvento.update);
 //router.delete('/tipoevento/:id', ctrlTipoEvento.delete);
 
 module.exports = router;
