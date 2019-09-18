679105ffa0cda78651231a444a41c5158111aad8
santiagogarcia97 <santiagogarcia97@outlook.com>
2019-09-17 21:09:25 -0300

    delete de jugadores

--- src/controllers/jugadores/delete.js
+++ src/controllers/jugadores/delete.js
@@ -1,37 +1,33 @@
-const { validationResult } = require('express-validator');
 const mongoose = require('mongoose');
 const sendRes = require('../../utils/sendResponse');
 const boom = require('@hapi/boom');
-const jugador = mongoose.model('jugador');
+const jugadorModel = mongoose.model('jugador');
 
 module.exports = async (req, res, next) => {
-    try {
-        let errors = validationResult(req); // Finds the validation errors in this request and wraps them in an object with handy functions
+  try {
 
-        if (!errors.isEmpty()) {
-            next(boom.badRequest('Error al validar los datos ingresados', errors));
-        }
-
-        await jugador.findById(req.params.id).
-        exec((err, result) => {
-            if (err) {
-                next(boom.badImplementation('Error al intentar eliminar el jugador', err));
-            }
-            else if (result) {
-                result.remove((err, result) => {
-                    if(err) {
-                        next(boom.badImplementation('Error al intentar eliminar el jugador', err));
-                    }
-                    else {
-                        sendRes(res, 200, 'Jugador eliminado con exito!', result);
-                    }
-                });
-            }
-            else {
-                sendRes(res, 200, 'El jugador que intenta eliminar no existe');
-            }
+    await jugadorModel.findById(req.params.id).
+    exec((err, result) => {
+      if (!err && result) {
+        result.remove((err, result) => {
+          if(!err && result) {
+            return sendRes(res, 200, 'Jugador eliminado con exito!', result);
+          }
+          else {
+            return next(boom.badImplementation('Error al intentar eliminar el jugador', err));
+          }
         });
-    } catch(err) {
-        return next(err)
-    }
+      }
+      else if(!err && !result) {
+        return sendRes(res, 200, 'El jugador que intenta eliminar no existe');
+      }
+      else {
+        return next(boom.badRequest('Error al intentar eliminar el jugador', err));
+      }
+    });
+
+  } catch(err) {
+    return next(err)
+  }
 }
+
