aa9f983bb5fdf790f0a91574b195ffca379869b6
santiagogarcia97 <santiagogarcia97@outlook.com>
2019-09-21 12:55:32 -0300

    update de dts

--- src/controllers/dts/update.js
+++ src/controllers/dts/update.js
@@ -1,43 +1,41 @@
 const mongoose = require('mongoose');
 const sendRes = require('../../utils/sendResponse');
 const boom = require('@hapi/boom');
-const jugadorModel = mongoose.model('jugador');
+const dtModel = mongoose.model('dt');
 
 module.exports = async (req, res, next) => {
   try {
 
-    await jugadorModel.findById(req.params.id).
+    await dtModel.findById(req.params.id).
     exec((err, result) => {
       if (!err && result) {
-        let jugador = {
+        let dtNew = {
           nombre: req.body.nombre || result.nombre,
           fechaNac: req.body.fechaNac || result.fechaNac,
-          equipo: req.body.equipo || (result.equipo === null)  ? undefined : result.equipo.str,
-          goles: req.body.goles || result.goles
+          equipo: req.body.equipo || (result.equipo === null)  ? undefined : result.equipo.str
         }
 
-        let error = jugadorModel.joiValidate(jugador);
+        let error = dtModel.joiValidate(dtNew);
         if(error.error)
           return next(boom.badRequest('Error al validar los datos ingresados', error.error))
 
         result.nombre = req.body.nombre || result.nombre;
         result.fechaNac = req.body.fechaNac || result.fechaNac;
-        result.equipo = req.body.equipo || result.equipo;
-        result.goles = req.body.goles || result.goles;
+        result.equipo = req.body.equipo || result.equipo
 
         result.save((err, result) => {
           if(!err && result){
-            return sendRes(res, 200, 'Jugador modificado con exito!', result);
+            return sendRes(res, 200, 'Director Tecnico modificado con exito!', result);
           } else{
-            return next(boom.badImplementation('Error al intentar actualizar el jugador', err));
+            return next(boom.badImplementation('Error al intentar actualizar el Director Tecnico', err));
           }
         });
       }
       else if(!err && !result) {
-        return sendRes(res, 200, 'El jugador no existe');
+        return sendRes(res, 200, 'El Director Tecnico no existe');
       }
       else {
-        return next(boom.badRequest('Error al intentar actualizar el jugador', err));
+        return next(boom.badRequest('Error al intentar actualizar el Director Tecnico', err));
       }
 
     });
