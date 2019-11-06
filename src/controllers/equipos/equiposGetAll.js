5872b9f19503f9e48a3fa857c462ccc8fe7fffc5
santiagogarcia97 <santiagogarcia97@outlook.com>
2019-11-06 13:49:01 -0300

    api equipos fixes

--- src/controllers/equipos/equiposGetAll.js
+++ src/controllers/equipos/equiposGetAll.js
@@ -3,24 +3,35 @@ const sendRes = require('../../utils/sendResponse');
 const boom = require('@hapi/boom');
 const equipoModel = mongoose.model('equipo');
 
+const populateOptions = [{
+  path: 'dt',
+  select: 'nombre'
+  },
+  {
+    path: 'jugador',
+    select: 'nombre',
+  }
+];
+
 module.exports = async (req, res, next) => {
   try{
 
-    await equipoModel.find({eliminado: false}, 'nombre jugadores dt escudo').
-      exec( (err, result) => {
+    await equipoModel.find({eliminado: false}, 'nombre jugadores dt escudo')
+      .populate(populateOptions)
+      .exec( (err, result) => {
 
-      if(!err && result){
-        if(result.length !== 0){
-          return sendRes(res, 200, 'Equipos recuperados con exito!', result);
+        if(!err && result){
+          if(result.length !== 0){
+            return sendRes(res, 200, 'Equipos recuperados con exito!', result);
+          }
+          else if (result.length === 0) {
+            return sendRes(res, 200, 'No existe ningun Equipo');
+          }
         }
-        else if (result.length === 0) {
-          return sendRes(res, 200, 'No existe ningun Equipo');
+        else{
+          return next(boom.badImplementation('Error al intentar recuperar Equipos', err));
         }
-      }
-      else{
-        return next(boom.badImplementation('Error al intentar recuperar Equipos', err));
-      }
-    });
+      });
   } catch(err){
     return next(err);
   }
--- src/controllers/equipos/equiposGetById.js
+++ src/controllers/equipos/equiposGetById.js
@@ -3,21 +3,32 @@ const sendRes = require('../../utils/sendResponse');
 const boom = require('@hapi/boom');
 const equipoModel = mongoose.model('equipo');
 
+const populateOptions = [{
+  path: 'dt',
+  select: 'nombre'
+},
+  {
+    path: 'jugador',
+    select: 'nombre',
+  }
+];
+
 module.exports = async (req, res, next) => {
   try {
 
-    await equipoModel.findById(req.params.id, 'nombre jugadores dt escudo eliminado').
-    exec((err, result) => {
-      if (!err && result) {
-        return sendRes(res, 200, 'Equipo recuperado con exito!', result);
-      }
-      else if(!err && !result) {
-        return sendRes(res, 200, 'El Equipo no existe');
-      }
-      else {
-        return next(boom.badRequest('Error al intentar recuperar Equipo', err));
-      }
-    });
+    await equipoModel.findById(req.params.id, 'nombre jugadores dt escudo eliminado')
+      .populate(populateOptions)
+      .exec((err, result) => {
+        if (!err && result) {
+          return sendRes(res, 200, 'Equipo recuperado con exito!', result);
+        }
+        else if(!err && !result) {
+          return sendRes(res, 200, 'El Equipo no existe');
+        }
+        else {
+          return next(boom.badRequest('Error al intentar recuperar Equipo', err));
+        }
+      });
   } catch(err) {
     return next(err)
   }
