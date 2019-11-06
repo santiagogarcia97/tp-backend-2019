a4c7b5326f7e2a2d66c709ece776b59fd760883b
santiagogarcia97 <santiagogarcia97@outlook.com>
2019-11-06 13:53:24 -0300

    api dts fixes

--- src/controllers/dts/dtsGetAll.js
+++ src/controllers/dts/dtsGetAll.js
@@ -3,23 +3,29 @@ const sendRes = require('../../utils/sendResponse');
 const boom = require('@hapi/boom');
 const dtModel = mongoose.model('dt');
 
+const populateOptions = {
+  path: 'equipo',
+  select: 'nombre escudo'
+};
+
 module.exports = async (req, res, next) => {
   try{
 
-    await dtModel.find(null, 'nombre fechaNac equipo').
-    exec( (err, result) => {
-      if(!err && result){
-        if(result.length !== 0){
-          return sendRes(res, 200, 'Directores Tecnicos recuperados con exito!', result);
+    await dtModel.find({eliminado: false}, 'nombre fechaNac equipo')
+      .populate(populateOptions)
+      .exec( (err, result) => {
+        if(!err && result){
+          if(result.length !== 0){
+            return sendRes(res, 200, 'Directores Tecnicos recuperados con exito!', result);
+          }
+          else if (result.length === 0) {
+            return sendRes(res, 200, 'No existe ningun Director Tecnico');
+          }
         }
-        else if (result.length === 0) {
-          return sendRes(res, 200, 'No existe ningun Director Tecnico');
+        else {
+          return next(boom.badImplementation('Error al intentar recuperar Directores Tecnicos', err));
         }
-      }
-      else {
-        return next(boom.badImplementation('Error al intentar recuperar Directores Tecnicos', err));
-      }
-    });
+      });
 
   } catch(err){
     return next(err);
