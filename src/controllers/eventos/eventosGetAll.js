e58c887c91b2a692543e8a792cf7b14d2f9c145f
santiagogarcia97 <santiagogarcia97@outlook.com>
2019-11-06 13:40:03 -0300

    api eventos fixes

--- src/controllers/eventos/eventosGetAll.js
+++ src/controllers/eventos/eventosGetAll.js
@@ -3,24 +3,40 @@ const sendRes = require('../../utils/sendResponse');
 const boom = require('@hapi/boom');
 const eventoModel = mongoose.model('evento');
 
+const populateOptions = [{
+    path: 'equipo',
+    select: 'nombre escudo'
+  },
+  {
+    path: 'jugador',
+    select: 'nombre',
+  },
+  {
+    path: 'tipo',
+    select: 'desc icono',
+  }
+];
+
 module.exports = async (req, res, next) => {
   try{
 
-    await eventoModel.find({eliminado: false}, 'partido equipo jugador tipo fechaHora').
-      exec( (err, result) => {
+    await eventoModel.find({eliminado: false}, 'partido equipo jugador tipo fechaHora')
+      .populate(populateOptions)
+      .exec( (err, result) => {
 
-      if(!err && result){
-        if(result.length !== 0){
-          return sendRes(res, 200, 'Eventos recuperados con exito!', result);
+        if(!err && result){
+          if(result.length !== 0){
+            return sendRes(res, 200, 'Eventos recuperados con exito!', result);
+          }
+          else if (result.length === 0) {
+            return sendRes(res, 200, 'No existe ningun Evento');
+          }
         }
-        else if (result.length === 0) {
-          return sendRes(res, 200, 'No existe ningun Evento');
+        else{
+          return next(boom.badImplementation('Error al intentar recuperar Eventos', err));
         }
-      }
-      else{
-        return next(boom.badImplementation('Error al intentar recuperar Eventos', err));
-      }
-    });
+      });
+
   } catch(err){
     return next(err);
   }
--- src/controllers/eventos/eventosGetById.js
+++ src/controllers/eventos/eventosGetById.js
@@ -3,21 +3,36 @@ const sendRes = require('../../utils/sendResponse');
 const boom = require('@hapi/boom');
 const eventoModel = mongoose.model('evento');
 
+const populateOptions = [{
+  path: 'equipo',
+  select: 'nombre escudo'
+},
+  {
+    path: 'jugador',
+    select: 'nombre',
+  },
+  {
+    path: 'tipo',
+    select: 'desc icono',
+  }
+];
+
 module.exports = async (req, res, next) => {
   try {
 
-    await eventoModel.findById(req.params.id, 'partido equipo jugador tipo fechaHora').
-    exec((err, result) => {
-      if (!err && result) {
-        return sendRes(res, 200, 'Evento recuperado con exito!', result);
-      }
-      else if(!err && !result) {
-        return sendRes(res, 200, 'El Evento no existe');
-      }
-      else {
-        return next(boom.badRequest('Error al intentar recuperar Evento', err));
-      }
-    });
+    await eventoModel.findById(req.params.id, 'partido equipo jugador tipo fechaHora eliminado')
+      .populate(populateOptions)
+      .exec((err, result) => {
+        if (!err && result) {
+          return sendRes(res, 200, 'Evento recuperado con exito!', result);
+        }
+        else if(!err && !result) {
+          return sendRes(res, 200, 'El Evento no existe');
+        }
+        else {
+          return next(boom.badRequest('Error al intentar recuperar Evento', err));
+        }
+      });
   } catch(err) {
     return next(err)
   }
